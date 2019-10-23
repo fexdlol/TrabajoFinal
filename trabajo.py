9a11bb9e332503f2ef3dd55c141a136ff7be3c18
Fernando Paredes <fe_paredesm@hotmail.com>
2019-10-23 08:27:35 -0500

    2do  avance

--- trabajo.py
+++ trabajo.py
@@ -6,9 +6,11 @@
 print("Este programa solo analizará un tipo de producto a la vez")
 
 name = input("Ingrese el nombre del producto: ")
+inter = input("Ingrese intervalo de tiempo: ")
 #Cantidad = can
 can = int(input("Ingrese la cantidad del producto a analizar: "))
-
+ingr = int(input("Ingrese el ingreso del producto: "))
+impt = int(input("Ingrese el importe: "))
 
 #Proceso
 
