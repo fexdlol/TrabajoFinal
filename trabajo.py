239d78c475a0cf5cec7cae2aeccfb137ca4cbe76
Mathias Villavicencio <mathiasvila_7@hotmail.com>
2019-10-29 08:40:17 -0500

    CASITODO

--- trabajo.py
+++ trabajo.py
@@ -3,37 +3,63 @@ os.system ("cls")
 
 #Inicio
 print("Este programa solo analizará un tipo de producto a la vez")
-print("El IGV tiene valor de 18.0%")
+
 name = input("Ingrese el nombre del producto: ")
+
+precioventa = float(input("Ingrese precio de venta del producto: "))
+while True:
+    if (precioventa <= 0):
+        precioventa = float(input(("Ingrese un precio de venta valido: ")))
+    else:
+        break
+
+#precioimport = es el precio real del producto cuando se importa
+precioimport = float(input("Ingrese precio fijo: "))
+while True:
+    if (precioimport <= 0):
+        precioimport = float(input(("Ingrese un precio fijo valido: ")))
+    else:
+        break
+
 inter = int(input("Ingrese intervalo de tiempo: "))
 while True:
     if (inter <= 0):
         inter = int(input(("Ingrese un intervalo de tiempo valido: ")))
     else:
         break
-#Cantidad = can
-can = int(input("Ingrese la cantidad del producto importado: "))
+
+cantidad = int(input("Ingrese la cantidad del producto importado: "))
 while True:
-    if (can <= 0):
-        can = int(input(("Ingrese una cantidad valida: ")))
-    else:
-        break
-ingr = float(input("Ingrese el ingreso del producto: "))
-while True:
-    if (ingr <= 0):
-        ingr = float(input(("Ingrese una cantidad valida del ingreso: ")))
-    else:
-        break
-vend = int(input("Ingrese la cantidad vendida: "))
-while True:
-    if (vend <= 0):
-        vend = int(input(("Ingrese una cantidad valida de la venta: ")))
+    if (cantidad <= 0):
+        cantidad = int(input(("Ingrese una cantidad valida: ")))
     else:
         break
 
+cantidadvend = int(input("Ingrese la cantidad vendida: "))
+while True:
+    if (cantidadvend <= 0):
+        cantidadvend = int(input(("Ingrese una cantidad valida de la venta: ")))
+    else:
+        break
 
+print("RECUERDE: El IGV tiene valor de 18.0% por el precio del producto")
 #Proceso
+ingreso = 0
+ingreso = cantidadvend * precioventa
+costo = cantidad * precioimport
+Ganancia = 0
+Ganancia = ingreso - costo
 
+#IGV
+IGV = precioventa * 0.18
+IGV = round(IGV,2)
+#total
+total = IGV + precioventa
+total = round(total,2)
+
+print("IGV: ", IGV)
+print ("Pago total es: ", total)
+print(Ganancia)
 
 
 #Salida
