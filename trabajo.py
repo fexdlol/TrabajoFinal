7d62045da56ba2696db3f2ffcf461d5aeb2e1aed
Mathias Villavicencio <mathiasvila_7@hotmail.com>
2019-10-29 07:35:52 -0500

    Actualizado

--- trabajo.py
+++ trabajo.py
@@ -1,16 +1,39 @@
 # NETO = TOTAL/(1+0.18)
 # IGV = TOTAL - NETO
+import os
 
+os.system ("cls")
 
 #Inicio
 print("Este programa solo analizará un tipo de producto a la vez")
 
 name = input("Ingrese el nombre del producto: ")
-inter = input("Ingrese intervalo de tiempo: ")
+inter = int(input("Ingrese intervalo de tiempo: "))
+while True:
+    if (inter <= 0):
+        inter = int(input(("Ingrese un intervalo de tiempo valido: ")))
+    else:
+        break
 #Cantidad = can
-can = int(input("Ingrese la cantidad del producto a analizar: "))
+can = int(input("Ingrese la cantidad del producto importado: "))
+while True:
+    if (can <= 0):
+        can = int(input(("Ingrese una cantidad valida: ")))
+    else:
+        break
 ingr = int(input("Ingrese el ingreso del producto: "))
-impt = int(input("Ingrese el importe: "))
+while True:
+    if (ingr <= 0):
+        ingr = int(input(("Ingrese una cantidad valida del ingreso: ")))
+    else:
+        break
+vend = int(input("Ingrese la cantidad vendida: "))
+while True:
+    if (vend <= 0):
+        vend = int(input(("Ingrese una cantidad valida de la venta: ")))
+    else:
+        break
+
 
 #Proceso
 
