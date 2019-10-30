65d7e846693064cfc1f799586303042e3d8dd18f
Mathias Villavicencio <mathiasvilla_7@hotmail.com>
2019-10-30 08:07:04 -0500

    Intervalo de Tiempo

---
+++ intervalotiempo.py
@@ -0,0 +1,32 @@
+import os
+os.system("cls")
+
+tiempo = input("¿Que intervalo de tiempo quiere eleguir?(semanal/mensual): ")
+while True:
+    if (tiempo == "semanal"):
+        print("Eliguío la opcion semanal")
+        break
+    if (tiempo == "mensual"):
+        print("Eliguío la opcion mensual")
+        break
+    else:
+        tiempo = input("Ingrese una opcion valida (semanal/mensual):")
+
+if (tiempo == "semanal"):
+    inter = int(input("Ingrese intervalo de tiempo: "))
+    while True:
+        if (inter <= 0):
+            inter = int(input(("Ingrese un intervalo de tiempo valido: ")))
+        for inter in range (1,6):
+            break
+        else:
+            break
+if (tiempo == "mensual"):
+    inter = int(input("Ingrese intervalo de tiempo: "))
+    while True:
+        if (inter <= 0):
+            inter = int(input(("Ingrese un intervalo de tiempo valido: ")))
+        else:
+            break
+       
+
