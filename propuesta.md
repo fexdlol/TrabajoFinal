7ea101ee5444f3e052a6d9e648fd286deb483be6
Fernando Paredes <fe_paredesm@hotmail.com>
2019-10-30 07:42:42 -0500

    actualizado

--- propuesta.md
+++ propuesta.md
@@ -9,7 +9,7 @@
 
 * Realizar un programa para el ambiente de ventas, este programa podrá calcular los ingresos de cada producto
     que se ingrese en dicho programa y a la vez mostrará la rentabilidad del producto ingresado.
-* El programa pedirá los intervalos de tiempo, ya sea diario, semanal o mensualmente.
+* El programa pedirá los intervalos de tiempo, ya sea semanal o mensualmente.
 * Pedirá los datos del producto ingresado (cantidad que se compró,cantidad que vendió mediante el intervalo dado y   el ingreso)
  
  ## División del proyecto:
--- trabajo.py
+++ trabajo.py
@@ -21,12 +21,24 @@ while True:
     else:
         break
 
+tiempo = input("¿Que intervalo de tiempo quiere eleguir?(semanal/mensual): ")
+while True:
+    if (tiempo == "semanal"):
+        print("Eliguío la opcion semanal")
+        break
+    if (tiempo == "mensual"):
+        print("Eliguío la opcion mensual")
+        break
+    else:
+        tiempo = input("Ingrese una opcion valida (semanal/mensual):")
+
 inter = int(input("Ingrese intervalo de tiempo: "))
 while True:
     if (inter <= 0):
         inter = int(input(("Ingrese un intervalo de tiempo valido: ")))
     else:
         break
+
 
 cantidad = int(input("Ingrese la cantidad del producto importado: "))
 while True:
